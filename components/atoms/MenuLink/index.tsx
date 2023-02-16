11808e2d1e5669b95c06c811e7d9a428f965b5fe
Abdulvokhid Ochilov <ochilov2050@gmail.com>
2023-02-16 16:43:51 +0500

    Navbar (#3)
    * navbar css
    
    * navbar links completed

--- components/atoms/MenuLink/index.tsx
+++ components/atoms/MenuLink/index.tsx
@@ -4,7 +4,10 @@ import { INavLink } from "@/types";
 
 export const MenuLink = ({ link, name }: INavLink) => {
   return (
-    <Link href={link} className="text-base pl-12 py-2 border-b">
+    <Link
+      href={link}
+      className="text-base pl-12 py-2 border-b md:text-lg md:border-0 md:px-4 md:hover:text-white md:hover:bg-primary md:hover:rounded-3xl md:hover:shadow"
+    >
       {name}
     </Link>
   );
--- components/templates/Navbar/index.tsx
+++ components/templates/Navbar/index.tsx
@@ -5,11 +5,23 @@ import Phone from "@/components/atoms/Phone";
 import Qabul from "@/components/atoms/Qabul";
 import Menu from "@/components/atoms/Menu";
 import MenuPanel from "@/components/molecules/MenuPanel";
+import { MenuLink } from "@/components/atoms/MenuLink";
+import { INavLink } from "@/types";
+
+const links = [
+  { link: "/", name: "Biz haqimizda" },
+  { link: "/", name: "Fakultetlar" },
+  { link: "/", name: "Universitet hayoti" },
+  { link: "/", name: "Kontrakt narxlari" },
+  { link: "/", name: "Talabalarga" },
+  { link: "/", name: "Ish joylari" },
+  { link: "/", name: "Murojaat uchun" },
+];
 
 const Navbar = () => {
   const [menu, setMenu] = useState(true);
   return (
-    <nav className="absolute w-full">
+    <nav className="absolute w-full shadow-md">
       <div className="bg-gradient-green">
         <div className="max-w-7xl mx-auto flex justify-between items-center p-3">
           <Logo />
@@ -24,9 +36,10 @@ const Navbar = () => {
         {!menu && <MenuPanel />}
       </div>
 
-      <div>
-        <div></div>
-      </div>
+      <div className="hidden md:flex flex-wrap justify-center max-w-7xl mx-auto py-3 gap-3">
+        {links.map(({ link, name }: INavLink) => (
+          <MenuLink key={name} link={link} name={name} />
+        ))}
     </nav>
   );
 };
